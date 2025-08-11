849e79386f10b5c3ec60df5a167ca5287c1c3503
liuyang meng <mengliuyang@163.com>
2025-08-11 17:58:37 +0800

    提交

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  base: '/',  // 确保是根路径
+  server: {
+    host: true, // 允许外部访问
+  }
 })
